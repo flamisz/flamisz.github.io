3278b0d5c7d168e1f348bc7fa38b77d47f41aec1
Zoltan Flamis <flamisz@gmail.com>
2019-03-30 16:01:55 +1300

    retire page

---
+++ webpack.mix.js
@@ -0,0 +1,37 @@
+let mix = require("laravel-mix");
+let tailwindcss = require("tailwindcss");
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g);
+  }
+}
+
+mix.postCss('src/styles.css', 'dist', [
+    require('tailwindcss'),
+])
+
+if (mix.inProduction()) {
+  mix.webpackConfig({
+    plugins: [
+      new PurgecssPlugin({
+
+        // Specify the locations of any files you want to scan for class names.
+        paths: glob.sync([
+          path.join(__dirname, "**/*.html")
+        ]),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+
+            // Specify the file extensions to include when scanning for
+            // class names.
+            extensions: ["html"]
+          }
+        ]
+      })
+    ]
+  });
+}
