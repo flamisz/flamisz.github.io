e311d57a4c1ce54e89c77dbccb2426bb120e66ed
Zoltan Flamis <flamisz@gmail.com>
2022-09-11 12:30:13 +1200

    wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -7,12 +7,13 @@ pagination:
 @extends('_layouts.main')
 
 @section('body')
-    IMAGE<br>
-    Zoltan Flamis<br>
-    twitter github
     @foreach ($pagination->items as $post)
         <p class="text-sm">{{ date('F j, Y', $post->date) }}</p>
         <h3><a href="{{ $post->getUrl() }}">{{ $post->title }}</a></h3>
         <div>{!! $post->excerpt() !!}</div>
     @endforeach
+
+    @if ($next = $pagination->next)
+        <a href="{{ $page->baseUrl }}{{ $next }}">See more »</a>
+    @endif
 @endsection
